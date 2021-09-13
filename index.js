8e3f6b89b89d34ae02675ae758a58049b63b6679
Alejandro Marcano <+>
2021-09-12 21:42:19 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const names = ["Ada", "Brendan", "Ali"];
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!!`);
+
+  }
+
+  return names;
+}
+
+writeCards(names);
+
+let countDown =22;
+while (countDown >= 0) {
+    console.log(countDown--)
+}
