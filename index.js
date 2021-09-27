979a2b5c7826b0e51c5a7a7077aa58bd1d1b66bd
Alejandro Marcano <+>
2021-09-26 22:36:40 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 // Code your solutions in this file
 
-const names = ["Ada", "Brendan", "Ali"];
-function writeCards(names) {
-  for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!!`);
+// function writeCards(x, y) {
+//   let arrayNames = []
+//   for (let i = 0; i < x.length; i++) {
+//   arrayNames.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`)
+//   }
+// return arrayNames
+// }
 
+function writeCards(x, y) {
+  let arrayNames = []
+  for (let i = 0; i < x.length; i++) {
+  arrayNames.push(`Thank you, ${x[i]}, for the wonderful ${y} gift!`)
   }
-
-  return names;
+return arrayNames
 }
 
-writeCards(names);
-
-let countDown =22;
-while (countDown >= 0) {
-    console.log(countDown--)
+function countDown(yo) {
+  while (yo >= 0){
+    console.log(yo--)
+  }
 }
+
+
+
+
+
+
+// const names = ["Ada", "Brendan", "Ali"];
+// function writeCards(names) {
+//   for (let i = 0; i < names.length; i++) {
+//     console.log(`Thank you, ${names[i]}, for the wonderful birthday gift!!`);
+
+//   }
+
+//   return names;
+// }
+
+// writeCards(names);
+
+// let countDown =22;
+// while (countDown >= 0) {
+//     console.log(countDown--)
+// }
